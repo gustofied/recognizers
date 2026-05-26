1eb7a22dbce6d0d8f92e383da50f0c1a67de62f6
gustofied <gustofied@gmail.com>
2026-05-26 20:02:28 +0200

    coroutine

--- src/recognizers/more/cr.py
+++ src/recognizers/more/cr.py
@@ -10,6 +10,34 @@ print(next(values))
 print(next(values)) 
 print(next(values))  
 
-
+print("-#-")
 for i in range_gen(10, 20, 2):
     print(i)
+
+print("-#-")
+def naturals():
+    n = 0
+    while True:
+        yield n
+        n += 1
+
+for i in naturals():
+    print(i)
+    if i > 12:
+        break
+
+print("-#-")
+
+# Dum dum fnction
+
+def echo():
+    print("lets go")
+    while True:
+        line = yield
+        print(line)
+
+gen = echo()
+gen.send(None)
+gen.send("Blah")
+gen.send("Blah")
+gen.send("Blah")
